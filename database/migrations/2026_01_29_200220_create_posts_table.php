500d07ee367578cc57a9252451f5dfa67e0a5a0e
thesnewby <stephen.j.newby@gmail.com>
2026-01-29 15:22:08 -0600

    part 5

---
+++ database/migrations/2026_01_29_200220_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id(); // or $table->increments('id');
+            $table->string('title'); // VARCHAR equivalent column
+            $table->mediumText('body'); // TEXT equivalent column
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}

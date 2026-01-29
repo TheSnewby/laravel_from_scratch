384c9c715abcd0125ec880d44959909af52739bc
thesnewby <stephen.j.newby@gmail.com>
2026-01-29 11:56:15 -0600

    initial upload. through most of video 4

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project Name</a>
+        </div>
+        <div id = "navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ url('/about') }}">About</a></li>
+                <li><a href="{{ url('/services') }}">Services</a></li>
+                <li><a href="{{ url('/posts') }}">Blog</a></li>
+            </ul>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/hello', function () {
+//     return '<h1>Hello, World!';
+// });
+
+// Route::get('/users/{id}/{name}', function($id, $name) {
+//     return "This is user ".$name." with an id of ".$id;
+// });
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
